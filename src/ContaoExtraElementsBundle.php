ed44d90218325b90f6e183fcb491071e291925f6
Artur Jozefiak <jozefiak@secra.de>
2025-09-17 17:15:09 +0100

    add version info

--- src/ContaoExtraElementsBundle.php
+++ src/ContaoExtraElementsBundle.php
@@ -1,10 +1,17 @@
 <?php
-
+// src/ContaoExtraElementsBundle.php
 namespace SolidWork\ContaoExtraElementsBundle;
 
+use Composer\InstalledVersions;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ContaoExtraElementsBundle extends Bundle
+final class ContaoExtraElementsBundle extends Bundle
 {
-//    public const VERSION = '0.6.2';
+    public function getVersion(): string
+    {
+        $pkg = 'solidwork/contao-extra-elements-bundle';
+        return \class_exists(InstalledVersions::class) && InstalledVersions::isInstalled($pkg)
+            ? (InstalledVersions::getPrettyVersion($pkg) ?? 'dev')
+            : 'dev';
+    }
 }
