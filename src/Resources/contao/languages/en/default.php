2c781c5ccb6b3d6d8778acc1195e96d1257dc337
Artur Jozefiak <jozefiak@secra.de>
2025-09-17 16:13:45 +0100

    correction to language file

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,3 +1,3 @@
 <?php
-
+$GLOBALS['TL_LANG']['CTE']['solidwork'] = 'SolidWork';
 $GLOBALS['TL_LANG']['CTE']['sowo_title_subtitle'] = ['Title + Subtitle', 'Outputs title and subtitle with configurable order.'];
