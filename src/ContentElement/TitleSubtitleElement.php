e73f1c740d093711f82ff43229432db54276b79f
Artur Jozefiak <jozefiak@secra.de>
2025-09-17 17:05:23 +0100

    add modern registration via attribute

--- src/ContentElement/TitleSubtitleElement.php
+++ src/ContentElement/TitleSubtitleElement.php
@@ -1,18 +1,22 @@
 <?php
-
+// src/ContentElement/TitleSubtitleElement.php
 namespace SolidWork\ContaoExtraElementsBundle\ContentElement;
 
 use Contao\ContentElement;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
 
-class TitleSubtitleElement extends ContentElement
+#[AsContentElement(
+    category: 'solidwork',
+    type: 'sowo_title_subtitle',
+    template: 'ce_title_subtitle'
+)]
+final class TitleSubtitleElement extends ContentElement
 {
-    protected $strTemplate = 'ce_title_subtitle';
-
     protected function compile(): void
     {
         $this->Template->sowo_title = (string)($this->sowo_title ?? '');
         $this->Template->sowo_subtitle = (string)($this->sowo_subtitle ?? '');
-        $this->Template->sowo_subtitle_position = (string)($this->sowo_subtitle_position ?? 'below'); // 'above'|'below'
-        $this->Template->sowo_headline_level = (string)($this->sowo_headline_level ?? 'h2');          // 'h1'..'h6'
+        $this->Template->sowo_subtitle_position = (string)($this->sowo_subtitle_position ?? 'below');
+        $this->Template->sowo_headline_level = (string)($this->sowo_headline_level ?? 'h2');
     }
 }
