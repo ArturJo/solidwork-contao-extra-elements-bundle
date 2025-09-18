54bddf6a1362985c89ece1f5f3e101f4ba67e189
Artur Jozefiak <jozefiak@secra.de>
2025-09-18 12:42:29 +0100

    Correct classes

--- src/ContaoExtraElementsBundle.php
+++ src/ContaoExtraElementsBundle.php
@@ -2,16 +2,8 @@
 // src/ContaoExtraElementsBundle.php
 namespace SolidWork\ContaoExtraElementsBundle;
 
-use Composer\InstalledVersions;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class ContaoExtraElementsBundle extends Bundle
+class ContaoExtraElementsBundle extends Bundle
 {
-    public function getVersion(): string
-    {
-        $pkg = 'solidwork/contao-extra-elements-bundle';
-        return \class_exists(InstalledVersions::class) && InstalledVersions::isInstalled($pkg)
-            ? (InstalledVersions::getPrettyVersion($pkg) ?? 'dev')
-            : 'dev';
-    }
 }
