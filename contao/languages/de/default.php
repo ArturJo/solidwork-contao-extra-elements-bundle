db56dd16d15f71352192723f005afa0918d012f9
Artur Jozefiak <jozefiak@secra.de>
2025-10-03 09:43:11 +0100

    fix(contao): update translation structure for SolidWork content elements

--- contao/languages/de/default.php
+++ contao/languages/de/default.php
@@ -1,7 +1,7 @@
 <?php
 
 // Kategorie (Gruppe) "SolidWork" in der Inhaltselement-Auswahl anzeigen
-$GLOBALS['TL_LANG']['CTE']['solidwork'] = 'SolidWork';
+$GLOBALS['TL_LANG']['CTE']['solidwork'] = ['SolidWork', ''];
 
 // Bezeichnung des Inhaltselements innerhalb der Kategorie
-$GLOBALS['TL_LANG']['CTE']['sowo_title_subtitle'] = ['Titel + Untertitel', 'Gibt Titel und Untertitel mit frei wählbarer Reihenfolge aus.'];
+$GLOBALS['TL_LANG']['CTE']['solidwork']['sowo_title_subtitle'] = ['Titel + Untertitel', 'Gibt Titel und Untertitel mit frei wählbarer Reihenfolge aus.'];
