2cffeec71aa9a78661fea8fca4b9c45d695ac41b
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-10-03 10:57:42 +0200

    Load the extensions in the AbstractBundle

--- src/ContaoExtraElementsBundle.php
+++ src/ContaoExtraElementsBundle.php
@@ -2,8 +2,14 @@
 // src/ContaoExtraElementsBundle.php
 namespace SolidWork\ContaoExtraElementsBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class ContaoExtraElementsBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
